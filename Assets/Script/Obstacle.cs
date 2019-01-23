bd462a76b13cdef820e6f133433695b3b9e25a60
hellno18 <michaeljack255@gmail.com>
2019-01-23 10:50:52 +0900

    Hit Effect
    -while hit, player give animation hit
    -Fixed size background
    -Fixed size UI
    -Deleted unnecessary files

--- Assets/Script/Obstacle.cs
+++ Assets/Script/Obstacle.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class Obstacle : MonoBehaviour {
     public int damage=1;
     private ScoreCalc scorecalc;
+    private Animator anim;
    
 
     // Use this for initialization
     void Start () {
         scorecalc = GameObject.Find("GameManager").GetComponent<ScoreCalc>();
-       
+        anim = GameObject.Find("Player").GetComponent<Animator>();
+        StartCoroutine(delayDead());
     }
 	
 	// Update is called once per frame
@@ -19,18 +21,26 @@ public class Obstacle : MonoBehaviour {
 	}
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.gameObject.tag == "Player")
         {
             collision.GetComponent<Player>().health -= damage;
+            anim.SetBool("Hit", true);
             scorecalc.heart.fillAmount -= 0.3333333333333333f;
             scorecalc.source.PlayOneShot(scorecalc.sfxHit, 0.5f);
             Debug.Log(collision.GetComponent<Player>().health);
             Destroy(gameObject);
+            
         }
-        if (collision.CompareTag("Destroy"))
+        if (collision.gameObject.tag=="Destroy")
         {
             Destroy(gameObject);
            
         }
+    }
+  
+    IEnumerator delayDead()
+    {
+        yield return new WaitForSeconds(0.1f);
+        anim.SetBool("Hit", false);
     }
 }
