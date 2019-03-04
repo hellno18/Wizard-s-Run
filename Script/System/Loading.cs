daef10147345a4b2ebafcc3e276638169ff263a8
Michael Jack <michaeljack255@gmail.com>
2019-03-04 17:41:55 +0900

    delete files
    -only show scripts and APK

---
+++ Script/System/Loading.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Loading : MonoBehaviour {
+    public Transform LoadingBar;
+    [SerializeField]
+    private float currentAmount;
+    [SerializeField]
+    private float speed;
+    private float volume;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        volume= PlayerPrefs.GetFloat("SoundState");
+        gameObject.GetComponent<AudioSource>().volume = volume;
+        if (currentAmount<100)
+        {
+            currentAmount += speed * Time.deltaTime;
+        }
+        else
+        {
+            Application.LoadLevel("MainMenu");
+        }
+
+        LoadingBar.GetComponent<Image>().fillAmount = currentAmount / 100;
+	}
+}
